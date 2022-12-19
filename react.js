68c39efb3d1e9aee02a865f4442c9b46ab0604a0
ameennm <ameennm014@gmail.com>
2022-12-19 15:55:06 +0530

    it other update

---
+++ react.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function react() {
+  return (
+    <div>react</div>
+  )
+}
+
+export default react
---
+++ second.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function second() {
+  return (
+    <div>second is the one</div>
+  )
+}
+
+export default second
